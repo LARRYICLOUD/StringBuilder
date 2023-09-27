ed97d430fb5197e5e8a54c42fb930e85ea45131c
LARRY MOSQUERA <larrysalsa@icloud.com>
2023-09-26 21:38:59 -0500

    Agregar archivos de proyecto.

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("YO SOY UN PROGRAMADOR Y EXPERTO EN REDES RECONOCIDO");
+            Console.WriteLine("#JAJAJA_FENOMENAL");
+            /* string numeros = "";
+
+             for (int i = 1; i<=100; i++)
+             {
+                 numeros += i + ",";
+
+             }
+             Console.WriteLine(numeros);*/
+           
+
+      var numeros = new StringBuilder();
+            for(int i = 1; i <= 100; i++)
+            {
+                numeros.Append(i);
+                numeros.Append(", ");
+            }
+
+           string cadenaNumeros = numeros.ToString();
+            Console.WriteLine(cadenaNumeros);
+        }
+    }
+}
